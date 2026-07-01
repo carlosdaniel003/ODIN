9318f1cf5bfec7e0706fd1a7881556a0b554eca0
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-01 10:18:17 -0400

    refactor: organiza inicialização do perfil Raspberry Pi 3

--- main_rpi.py
+++ main_rpi.py
@@ -2,6 +2,12 @@ import tkinter as tk
 
 from src.platform.raspberry_pi3_profile import RaspberryPi3ODINApp
 
-root = tk.Tk()
-RaspberryPi3ODINApp(root)
-root.mainloop()
+
+def main() -> None:
+    root = tk.Tk()
+    RaspberryPi3ODINApp(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
