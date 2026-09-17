13c97f48595d7525283ccd194a0676dfd642b97d
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-09-17 15:59:30 -0400

    Wire smooth F2 orientation editor workflow

--- main_rpi.py
+++ main_rpi.py
@@ -65,6 +65,9 @@ from src.platform.f2_tracking_orientation_precision_roi_editor import (  # noqa:
 from src.platform.f2_tracking_orientation_precision_interaction_fix import (  # noqa: E402
     instalar_ajustes_interacao_precisao_orientacao_f2,
 )
+from src.platform.f2_tracking_orientation_editor_workflow import (  # noqa: E402
+    instalar_fluxo_interacao_editor_orientacao_f2,
+)
 from src.platform.f2_tracking_orientation_live_capture import (  # noqa: E402
     instalar_captura_guiada_referencias_orientacao_f2,
 )
@@ -145,6 +148,7 @@ instalar_editor_vertices_referencias_orientacao_f2()
 instalar_transformacao_base_editor_vertices_orientacao_f2()
 instalar_precisao_rois_referencias_orientacao_f2()
 instalar_ajustes_interacao_precisao_orientacao_f2()
+instalar_fluxo_interacao_editor_orientacao_f2()
 instalar_captura_guiada_referencias_orientacao_f2()
 instalar_presenca_por_referencias_angulares_f2()
 instalar_reaquisicao_durante_hold_f2()
